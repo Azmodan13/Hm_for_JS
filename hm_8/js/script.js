27ecb64f15e1e8ea20163ed0c137c893061d9743
Dima Taran <dimataran@MacBook-Air-Dima.local>
2022-06-28 06:36:56 +0300

    Hm 8

--- hm_8/js/script.js
+++ hm_8/js/script.js
@@ -1,62 +1,168 @@
 
-const start = document.querySelector('.btn');
-const wraper = document.querySelector('.wraper')
-
-
-function createItems(){
+const start = document.createElement('button');
+start.className = 'btn';
+start.innerHTML = 'START';
+const wraper = document.createElement('div')
+wraper.className = 'wraper';
+document.body.append(wraper);
+wraper.append(start);
 
     // *  Создаем игровое поле
-    let game = document.createElement('div');
+function createItems(){
+    const game = document.createElement('div');
     game.className = 'game';
-    let item = document.createElement('div');
-    item.className = 'item';
-
     for (let i = 0; i < 9; i++){
         let item = document.createElement('div');
         item.className = 'item';
         game.append(item);
     }
     wraper.append(game);
-    const clicked = document.getElementsByClassName('item');
+}
 
-
-    // * какой игрок ходит
+    let clicked = document.getElementsByClassName('item');
+    const game = document.getElementsByClassName('game');
+    
     let player = true ;
     function players() {
         if(player === true){
             player = false;
             
-            return 'player_1'
+            return 'X'
         }else{
             player = true;
             
-            return 'player_2'
+            return 'O'
         }
     }
 
 
     //* клик на ечейку
-    
-    for (var i = 0; i < clicked.length; i++) {
+    function checked(){
+        for (var i = 0; i < clicked.length; i++) {
         clicked[i].addEventListener("click", function() {
+        this.innerText = players();
 
-        this.classList.add(players())
-
-
-
+            checkArr(getArr());
         }, {once : true});
     }
-
-    console.log(clicked);
-
-
 }
+
+
+
 
     //* старт игры
 start.addEventListener('click', function(){
     createItems();
+    checked();
     start.remove()
 });
 
+function getArr() {
+    for (var i = 0; i < clicked.length; i++) {
+            var arr = Array.from(clicked);
+            var mapP = arr.map(t => {return t.innerText});   
+            return mapP
+        
+    }
+}
+
+function checkArr(arr){
+    counter = 1;
+    for(let i = 0; i < arr.length; i++){
+        if ( arr[0] == "X" && arr[1] == 'X' && arr[2] == 'X' ||
+    arr[3] == "X" && arr[4] == 'X' && arr[5] == 'X' ||
+    arr[6] == "X" && arr[7] == 'X' && arr[8] == 'X' ||
+    arr[0] == "X" && arr[3] == 'X' && arr[6] == 'X' ||
+    arr[1] == "X" && arr[4] == 'X' && arr[7] == 'X' ||
+    arr[2] == "X" && arr[5] == 'X' && arr[8] == 'X' ||
+    arr[0] == "X" && arr[4] == 'X' && arr[8] == 'X' ||
+    arr[6] == "X" && arr[4] == 'X' && arr[2] == 'X' ){
+		return tryAgain('Player X won')
+    }
+    
+    if ( arr[0] == "O" && arr[1] == 'O' && arr[2] == 'O' ||
+    arr[3] == "O" && arr[4] == 'O' && arr[5] == 'O' ||
+    arr[6] == "O" && arr[7] == 'O' && arr[8] == 'O' ||
+    arr[0] == "O" && arr[3] == 'O' && arr[6] == 'O' ||
+    arr[1] == "O" && arr[4] == 'O' && arr[7] == 'O' ||
+    arr[2] == "O" && arr[5] == 'O' && arr[8] == 'O' ||
+    arr[0] == "O" && arr[4] == 'O' && arr[8] == 'O' ||
+    arr[6] == "O" && arr[4] == 'O' && arr[2] == 'O' ){
+        return tryAgain('Player O won')
+        
+    }
+    
+    if 
+    ( arr[0] == "X" && arr[1] == 'O' && arr[2] == 'X' &&
+    arr[3] == "O" && arr[4] == 'X' && arr[5] == 'X' &&
+    arr[6] == "O" && arr[7] == 'X' && arr[8] == 'O' ||
+
+    arr[0] == "O" && arr[1] == 'X' && arr[2] == 'X' &&
+    arr[3] == "X" && arr[4] == 'O' && arr[5] == 'O' &&
+    arr[6] == "X" && arr[7] == 'O' && arr[8] == 'X' ||
+
+    arr[0] == "X" && arr[1] == 'O' && arr[2] == 'X' &&
+    arr[3] == "O" && arr[4] == 'O' && arr[5] == 'X' &&
+    arr[6] == "X" && arr[7] == 'X' && arr[8] == 'O' ||
+
+    arr[0] == "X" && arr[1] == 'O' && arr[2] == 'O' &&
+    arr[3] == "O" && arr[4] == 'X' && arr[5] == 'X' &&
+    arr[6] == "X" && arr[7] == 'X' && arr[8] == 'O' ||
+
+    arr[0] == "X" && arr[1] == 'O' && arr[2] == 'X' &&
+    arr[3] == "X" && arr[4] == 'O' && arr[5] == 'X' &&
+    arr[6] == "O" && arr[7] == 'X' && arr[8] == 'O' ||
+
+    arr[0] == "O" && arr[1] == 'O' && arr[2] == 'X' &&
+    arr[3] == "X" && arr[4] == 'X' && arr[5] == 'O' &&
+    arr[6] == "O" && arr[7] == 'X' && arr[8] == 'X' ||
+
+    arr[0] == "X" && arr[1] == 'X' && arr[2] == 'O' &&
+    arr[3] == "O" && arr[4] == 'X' && arr[5] == 'X' &&
+    arr[6] == "X" && arr[7] == 'O' && arr[8] == 'O' ||
+
+    arr[0] == "X" && arr[1] == 'O' && arr[2] == 'X' &&
+    arr[3] == "X" && arr[4] == 'X' && arr[5] == 'O' &&
+    arr[6] == "O" && arr[7] == 'X' && arr[8] == 'O' ||
+
+    arr[0] == "O" && arr[1] == 'X' && arr[2] == 'X' &&
+    arr[3] == "X" && arr[4] == 'O' && arr[5] == 'O' &&
+    arr[6] == "O" && arr[7] == 'X' && arr[8] == 'X' ||
 
 
+    arr[0] == "O" && arr[1] == 'X' && arr[2] == 'O' &&
+    arr[3] == "X" && arr[4] == 'X' && arr[5] == 'O' &&
+    arr[6] == "X" && arr[7] == 'O' && arr[8] == 'X' ||
+
+
+    arr[0] == "X" && arr[1] == 'O' && arr[2] == 'X' &&
+    arr[3] == "X" && arr[4] == 'O' && arr[5] == 'O' &&
+    arr[6] == "O" && arr[7] == 'X' && arr[8] == 'X' ||
+
+
+    arr[0] == "O" && arr[1] == 'X' && arr[2] == 'O' &&
+    arr[3] == "X" && arr[4] == 'O' && arr[5] == 'X' &&
+    arr[6] == "X" && arr[7] == 'O' && arr[8] == 'X'
+    )
+    return tryAgain('Draw')
+    } 
+
+    
+}
+
+function tryAgain(winner){
+    const wrap = document.createElement('div');
+    wrap.className = 'wrap';
+    const btn = document.createElement('button');
+    btn.className = 'btn';
+    btn.innerText = 'Try again';
+    const text = document.createElement('h1');
+    text.className = 'winner';
+    text.innerHTML = winner;
+    wrap.append(text, btn);
+    wraper.append(wrap);
+    btn.addEventListener('click', function(){
+        location.reload();
+
+    });
+}
