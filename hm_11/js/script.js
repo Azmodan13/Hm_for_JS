dd405b9a0996dcb3e76633e073da7a51a0fd48d8
Dima Taran <dimataran@MacBook-Air-Dima.local>
2022-07-06 21:05:54 +0300

    add 11 Hm

--- hm_11/js/script.js
+++ hm_11/js/script.js
@@ -1,39 +1,45 @@
-const deley = ms => {
-    return new Promise(r => setTimeout(() => r(), ms))
-};
 
 
 document.querySelector('.btn').addEventListener('click', function() {
-    console.log('test')
     let user = document.querySelector('.input').value;
-    fetchAssyncTodos(user)
+    if (user.length <= 3){
+        console.log('err')
+    }else {
+        fetchAssyncTodos(user)
+    }
+})
+
+document.querySelector('.clear').addEventListener('click', function() {
+    let item = document.querySelectorAll('.item');
+    item.forEach(element => {
+        element.remove()
+    });
 })
 
 const url = "https://api.github.com/users";
 
 
 async function fetchAssyncTodos (id) {
-    console.log('Fetch todo started...')
     try {
-        await deley(1000)
         const response = await fetch(`${url}/${id}`)
         const data = await response.json()
-        console.log(data)
-        createElement(data.avatar_url, data.login, data.public_repos)
+        if(response.ok === false){
+            return err
+        }
 
+        createElement(data.avatar_url, data.login, data.public_repos, data.html_url)
     } catch (e) {
-        console.error(e)
-    } finally {
-        await deley(2000)
-        console.log('finally')
+        // console.error(e)
+        createError('https://miro.medium.com/max/800/1*hFwwQAW45673VGKrMPE2qQ.png')
     }
 }
 
-function createElement(foto, userLog, description) {   
+function createElement(foto, userLog, description, link) {   
     let wraper = document.querySelector('.wraper')
     
     const item = document.createElement('div');
     item.className = 'item'
+    
 
     const img = document.createElement('img');
     img.className = 'img'
@@ -47,9 +53,52 @@ function createElement(foto, userLog, description) {
     login.className = 'login'
     login.innerHTML = userLog
 
+    const profile = document.createElement('a');
+    profile.className = 'profile'
+    profile.innerHTML = 'Profile'
+    profile.href = link
+
+    const remove = document.createElement('button');
+    remove.className = 'remove'
+    remove.innerHTML = 'Remove'
+
+    remove.addEventListener('click', function () {
+        item.classList.remove('visible')
+        setTimeout(() => {item.remove(); }, 1000);
+    })
+
+    setTimeout(() => {item.classList.add('visible')}, 300);
 
 
-    item.append(img, login, descr)
+    item.append(img, login, profile, descr, remove)
     wraper.append(item)
 }
 
+function createError(foto) {   
+    let wraper = document.querySelector('.wraper')
+    
+    const item = document.createElement('div');
+    item.className = 'item'
+    
+
+    const img = document.createElement('img');
+    img.className = 'img'
+    img.src = foto
+ 
+
+
+    const remove = document.createElement('button');
+    remove.className = 'remove'
+    remove.innerHTML = 'Remove'
+
+    remove.addEventListener('click', function () {
+        item.classList.remove('visible')
+        setTimeout(() => {item.remove(); }, 1000);
+    })
+
+    setTimeout(() => {item.classList.add('visible')}, 300);
+
+
+    item.append(img,remove)
+    wraper.append(item)
+}
